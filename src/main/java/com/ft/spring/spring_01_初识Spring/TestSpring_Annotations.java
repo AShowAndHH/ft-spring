1d7d195d471ae13d83bb205892159e782fce266e
mskj-ft <806244264@qq.com>
2022-09-29 12:52:04 +0800

    spring_01_初识Spring

---
+++ src/main/java/com/ft/spring/spring_01_初识Spring/TestSpring_Annotations.java
@@ -0,0 +1,29 @@
+package com.ft.spring.spring_01_初识Spring;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @Description 读取 Spring 配置文件（注解读取）
+ * @Author fangt
+ * @CreateTime 2022/9/28 12:17
+ */
+
+@Slf4j
+// 使用标准的JUnit @RunWith注释来告诉JUnit使用Spring TestRunner
+@RunWith(SpringJUnit4ClassRunner.class)
+// 加载配置文件，否则无法注入
+@ContextConfiguration(locations = {"classpath:applicationContext_01_初识Spring.xml"})
+public class TestSpring_Annotations {
+    @Autowired
+    private SpringBean bean;
+
+    @Test
+    public void test() {
+        log.info("{}", bean);
+    }
+}
---
+++ src/main/java/com/ft/spring/spring_01_初识Spring/TestSpring_Manual.java
@@ -0,0 +1,30 @@
+package com.ft.spring.spring_01_初识Spring;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @Description 读取 Spring 配置文件（手动读取）
+ *  1、读取xml配置文件：new ClassPathXmlApplicationContext("applicationContext.xml");
+ *  2、读取类配置文件（@Configuration 或者 @Component）：new AnnotationConfigApplicationContext(Config01.class, Config02.class, Config03.class);
+ * @Author fangt
+ * @CreateTime 2022/9/28 12:17
+ */
+@Slf4j
+public class TestSpring_Manual {
+    private ConfigurableApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext_01_初识Spring.xml");
+
+    private SpringBean bean = applicationContext.getBean(SpringBean.class);
+
+    @Test
+    public void test() {
+        // 这里显式注册一个钩子函数；在Spring项目启动时，Spring会自己注册一个钩子函数
+        // 1、注册的时候，不存在才注册
+        // 2、作用：Spring定义的钩子函数中会去执行, DisposableBean.destory()和被 @PreDestroy 修饰的方法
+        applicationContext.registerShutdownHook();
+
+        log.info("{}", bean);
+    }
+}
